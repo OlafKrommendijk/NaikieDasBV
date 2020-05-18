57a1e4b866402bafaf79b933354075b138c366d2
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2020-05-18 10:42:37 +0200

    Start of homepage

---
+++ pagesInclude/homepage.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <link rel="stylesheet" href="./assets/css/homepageStyle.css">
+</head>
+<body>
+
+<div id="page-wrapper">
+    <h1>Homepage test</h1>
+</div>
+
+</body>
+</html>
