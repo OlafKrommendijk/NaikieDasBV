1a5f384a39c6b78d9e470baf81da4a1292eff265
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2020-05-18 11:10:43 +0200

    layout for a standard customer completed

--- pagesInclude/homepage.php
+++ pagesInclude/homepage.php
@@ -1,15 +1,39 @@
 <?php
 ?>
-<!DOCTYPE html>
-<html lang="nl">
 <head>
     <link rel="stylesheet" href="./assets/css/homepageStyle.css">
 </head>
-<body>
 
 <div id="page-wrapper">
-    <h1>Homepage test</h1>
+    <div class="left-bar">
+        <p>Zoek een vacature</p>
+        <form action="/action_page.php">
+            <input type="text" placeholder="Vacature zoeken" name="search">
+            <button type="submit">Zoeken</button>
+        </form>
+
+        <label for="categorise">Categoriseer:</label>
+        <br>
+        <select id="categorise">
+            <option value="branch">Filiaal</option>
+            <option value="job">Functie</option>
+        </select>
+        <br>
+        <label for="filter">Filter:</label>
+        <br>
+        <select id="filter">
+            <option value="branch">Filiaal</option>
+            <option value="job">Functie</option>
+        </select>
+    </div>
+
+    <div class="right-bar">
+        <div class="offer">
+            <p>Offer Title</p>
+        </div>
+    </div>
 </div>
+
 
 </body>
 </html>
