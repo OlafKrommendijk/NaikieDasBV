711f33552ad39c9552715f7099e110f63c130e27
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2020-05-18 10:08:37 +0200

    Start project vacatureoverzicht

---
+++ DBconfig.php
@@ -0,0 +1,13 @@
+<?php
+DEFINE("DB_USER", "root");
+DEFINE("DB_PASS", "");
+
+try {
+    $db = new PDO("mysql:host=localhost;dbname=fff",DB_USER,DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+//Laad de benodigde bestanden in
+include_once('header.php');
+include_once('pages/homePage.php');
+include_once('footer.php');
