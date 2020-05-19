063034882ddb8d65180f6448fff8c53daf738329
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2020-05-19 10:12:51 +0200

    Customer is able to react to an joboffer

--- pagesFunctions/jobOffer.php
+++ pagesFunctions/jobOffer.php
@@ -1,14 +1,10 @@
 <?php
 
-function getAllOffers(){
 
-    try {
-        $db = new PDO("mysql:host=localhost;dbname=naikiedasbv",DB_USER,DB_PASS);
-        $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e) {
-        echo $e->getMessage();
-    }
+function getAllOffers()
+{
+    require_once '../pagesInclude/DBconfig.php';
+    $db = DBConnection();
 
 
     $sql = "SELECT * FROM joboffer;";
@@ -19,3 +15,55 @@ function getAllOffers(){
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function getOfferById($oId)
+{
+    require_once '../pagesInclude/DBconfig.php';
+    $db = DBConnection();
+
+    $query = "SELECT * FROM joboffer WHERE jobOfferId = ?";
+    $stmt = $db->prepare($query);
+    $stmt->execute(array($_GET['id']));
+
+    return $stmt->fetch();
+}
+
+function offerReaction()
+{
+    require_once '../pagesInclude/DBconfig.php';
+    $db = DBConnection();
+
+    if (empty($_POST['motivation']) || empty($_FILES['fileToUpload'])) {
+        echo "<script>alert('Vergeet niet de verplichte velden in te vullen');</script>";
+        echo "<script>window.location.href = '../pagesInclude/homepage.php';</script>";
+        exit;
+    }
+    if (!isset($_SESSION['userId'])) {
+        echo "<script>alert('U moet ingelogd zijn');</script>";
+        echo "<script>window.location.href = '../pagesInclude/homepage.php';</script>";
+        exit;
+    }
+
+
+    $target_dir = "../assets/uploads/";
+    $cv = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+    //Trying to upload file
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $cv)) {
+        echo "<script>alert('bestand is geupload');</script>";
+    } else {
+        echo "<script>alert('Bestand uploaden mislukt');</script>";
+        echo "<script>window.location.href = '../pagesInclude/homepage.php';</script>";
+        exit;
+    }
+
+    $motivation = htmlspecialchars($_POST['motivation']);
+    $cv = $_FILES['fileToUpload'];
+    $offerId = htmlspecialchars($_POST['offerId']);
+    $userId = $_SESSION['userId'];
+
+    $query = "INSERT INTO offerreaction (idUser, idJoboffer, motivation, cv )  VALUES ('$userId', '$offerId', '$motivation', '$cv')";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+
+
+}
---
+++ pagesInclude/offerPage.php
@@ -0,0 +1,51 @@
+<?php
+require_once('header.php');
+?>
+
+<head>
+    <link rel="stylesheet" href="../assets/css/offerPageStyle.css">
+</head>
+<body>
+
+<div id="page-wrapper">
+
+
+
+    <?php
+    $oId = $_GET['id'];
+    require_once '../pagesFunctions/jobOffer.php';
+    $offer = getOfferById($oId);
+
+
+    echo '<div class="offerBox">';
+    echo '<div class="offer">';
+    echo '<div class="offerTitle">';
+    echo $offer['name'];
+    echo '</div><br>';
+    echo '<div class="offerDescription">';
+    echo $offer['description'];
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+
+    require_once '../pagesFunctions/jobOffer.php';
+    $jobOfferId = $offer['jobofferID'];
+
+    if (isset($_POST['submit'])) {
+        offerReaction();
+    }
+    ?>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <h3>Reageren op de vacature</h3>
+        <p>CV</p>
+        <input type="file" name="fileToUpload" id="fileToUpload" accept=".pdf,.doc">
+        <br>
+        <p>Motivatie</p>
+        <input title="motivation" type="text" name="motivation" id="motivation">
+        <br>
+        <input type="hidden" id="offerId" name="offerId" value="<?php echo $jobOfferId;?>">
+        <input type="submit" value="Verstuur" name="submit">
+    </form>
+    
+</div>
